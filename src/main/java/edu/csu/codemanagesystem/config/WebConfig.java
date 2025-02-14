394f6c37505d020ba6c342d053ec6a8166aee61b
z <2647461091@qq.com>
2025-02-14 18:06:11 +0800

    feat: 允许跨域请求

---
+++ src/main/java/edu/csu/codemanagesystem/config/WebConfig.java
@@ -0,0 +1,18 @@
+package edu.csu.codemanagesystem.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://example.com") // 允许来自 http://example.com 的请求
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
